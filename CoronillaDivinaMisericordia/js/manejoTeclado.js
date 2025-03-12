58c8f25893f5a0ea03611c18e26a777fda9d06bc
Jesus García <jeigar2@gmail.com>
2025-03-12 01:08:02 +0100

    corrigiendo la visualizacion en dispositivos moviles para la coronilla

--- CoronillaDivinaMisericordia/js/manejoTeclado.js
+++ CoronillaDivinaMisericordia/js/manejoTeclado.js
@@ -6,7 +6,7 @@ function handleKeyboardEvent(event) {
 
     switch(event.key) {
         case ' ':
-            flashEffect(event.target);
+            //flashEffect(event.target);
         case 'ArrowDown':
             event.preventDefault();
             moveForward();
@@ -19,7 +19,33 @@ function handleKeyboardEvent(event) {
             event.preventDefault();
             stopAllAudios();
             break;
+        case 'Home':
+            event.preventDefault();
+            moveToFirstElement();
+            break;
+        case 'End':
+            event.preventDefault();
+            moveToLastElement();
+            break;
     }
+}
+
+function moveToFirstElement() {
+    if (elements.length === 0) {
+        initializeElements();
+    }
+    currentIndex = 0;
+    elements[currentIndex].scrollIntoView({ behavior: 'smooth' });
+    simulateEvent(elements[currentIndex], 'mouseenter');
+}
+
+function moveToLastElement() {
+    if (elements.length === 0) {
+        initializeElements();
+    }
+    currentIndex = elements.length - 1;
+    elements[currentIndex].scrollIntoView({ behavior: 'smooth' });
+    simulateEvent(elements[currentIndex], 'mouseenter');
 }
 
 // Añadimos el listener para eventos de teclado
